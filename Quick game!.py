451d4ffd1d7b9a3bdffa2444c4ea9085d9edf9cb
IbbyK <KILMISTERIbr@hardenhuish.wilts.sch.uk>
2017-03-24 12:07:30 +0000

    Game

---
+++ Quick game!.py
@@ -0,0 +1,28 @@
+import time
+import sys
+print("Hello")
+Ready = input("Are you ready")
+if Ready == "no":
+    print("Ok bye bye")
+if Ready == "No":
+    print("Ok bye bye")
+if Ready == "Yes":
+    print("The game is starting")
+    Name = input("Please enter a name for your player")
+    time.sleep(1.75)
+    print(Name+"has been made!")
+    time.sleep(1.75)
+    print("I hope you have fun!")
+    time.sleep(1.75)
+    print("Day 1")
+    time.sleep(1.75)
+    print("Today is the day you can show your game you have been working on to your teacher")
+    time.sleep(1.75)
+    print("This could get you a place in the top class!")
+    time.sleep(1.75)
+    print("The thing is you have a really bad headache and feel sick")
+    Choice1 = input("Do you go to school?")
+    if choice1 == "Yes":
+        print("You start to pack your bags")
+        print("You are a bit unsure about your first lesson.\nDo you find your timetable")
+        time.sleep(1.75)
